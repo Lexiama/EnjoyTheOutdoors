065bc56fa7d6321b536fd0576494ea4839731e0b
lexiama <alexia.mckoy@gmail.com>
2023-11-14 18:48:14 -0500

    updated

--- scripts/parks.js
+++ scripts/parks.js
@@ -4,30 +4,40 @@ let searchInput = document.querySelector("#searchInput");
 let searchForm = document.querySelector(".search");
 let parksContainer = document.querySelector(".parksContainer");
 
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // stop reloading the form
+    const type = searchType.value;
+    const input = searchInput.value;
+  
+    //console.log(type, input);
+  });
+
+
 // loop through locations array and put in locations select input
 locationsArray.forEach((location) => {
   locationSelect.innerHTML += `<option value='${location}'>${location}</option>`;
 });
 
-// adding event listener so that when we change the search type, js will know
-// and we can hide and show the locations select input
+// adding event listener so that when I can change the search type, js will know
+// and I can hide and show the locations select input
 searchType.addEventListener("change", () => {
   if (searchType.value === "location") {
-    locationSelect.style.display = "inline";
-  } else {
+    locationSelect.style.display = "inline";  
+  } else {                                     // hide and showing the location
     locationSelect.style.display = "none";
   }
 });
 
 locationSelect.addEventListener("change", () => {
-  const filteredParks = nationalParksArray.filter((park) =>
+  const filteredParks = nationalParksArray.filter((park) =>          
     park.State.includes(locationSelect.value)
   );
 
   for (let i = 0; i < filteredParks.length; i++) {
     const park = filteredParks[i];
     parksContainer.innerHTML = `
-        <div class="parkCard">
+        <div class="parkCard">                               
            <h3>Location Name: ${park.LocationName}</h3> 
            <p>Address: ${park.Address}</p>
            <p>City: ${park.City}</p>
@@ -35,15 +45,9 @@ locationSelect.addEventListener("change", () => {
            <p>Type: ${park.Location.type}</p>
         </div>
     `;
-  }
+  }       // the back ticks are helping me because the "" wont work
 });
 
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault(); // stop reloading of form
-  const type = searchType.value;
-  const input = searchInput.value;
 
-  console.log(type, input);
-});
 
-console.log(nationalParksArray);
+
