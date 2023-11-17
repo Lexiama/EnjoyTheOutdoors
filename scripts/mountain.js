c21c02cb754d9e8992f7c5c0f1d58f14ffe7687b
lexiama <alexia.mckoy@gmail.com>
2023-11-17 13:38:45 -0500

    updated

--- scripts/mountain.js
+++ scripts/mountain.js
@@ -1,22 +1,44 @@
-const mountainsSelect = document.getElementById("mountains");
-const mountainContainer = document.querySelector(".mountainContainer");
+"use strict";
 
-// looping through mountains array and put in mountains select input
-mountainsArray.forEach((mountain) => {
-  mountainsSelect.innerHTML += `<option value='${mountain.name}'>${mountain.name}</option>`;
-});
+window.onload = () => {
+  const mountainsSelect = document.getElementById("mountains");
+  const mountainContainer = document.querySelector(".mountainContainer");
 
-mountainsSelect.addEventListener("change", () => {
-  const value = mountainsSelect.value;
-  const mountain = mountainsArray.find((mt) => mt.name === value);
-  console.log(mountain);
+  // looping through mountains array and put in mountains select input
+  mountainsArray.forEach((mountain) => {
+    mountainsSelect.innerHTML += `<option value='${mountain.name}'>${mountain.name}</option>`;
+  });
 
-  mountainContainer.innerHTML = `
-  <div class="parkCard">                               
-     <p>Mountain Name: ${mountain.name}</p> 
-     <p>Description: ${mountain.description}</p>
-     <p>Elevation: ${mountain.elevation}</p>
-     <img src='../images/${mountain.img}' alt="mountain" />
-  </div>
-`;
-});
+  mountainsSelect.addEventListener("change", mountainsSelectOnchange);
+
+  async function mountainsSelectOnchange() {
+    const value = mountainsSelect.value;
+    const mountain = mountainsArray.find((mt) => mt.name === value);
+    const sunsetData = await getSunsetForMountain(
+      mountain.coords.lat,
+      mountain.coords.lng
+    );
+    console.log(mountain);
+    console.log(sunsetData);
+
+    mountainContainer.innerHTML = `
+    <div class="parkCard">                               
+       <p>Mountain Name: ${mountain.name}</p> 
+       <p>Description: ${mountain.desc}</p>
+       <p>Elevation: ${mountain.elevation}</p>
+       <p>Sunrise Time: ${sunsetData.results.sunrise}</p>
+       <p>Sunset Time: ${sunsetData.results.sunset}</p>
+       <img src= 'images/${mountain.img}' alt="mountain" />
+    </div>
+  `;
+  }
+
+  // function that can "fetch" the sunrise/sunset times
+  async function getSunsetForMountain(lat, lng) {
+    let response = await fetch(
+      `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
+    );
+    let data = await response.json();
+    return data;
+  }
+};
--- scripts/parks.js
+++ scripts/parks.js
@@ -1,46 +1,71 @@
 "use strict";
 
-let searchType = document.querySelector("#searchType");
-let locationSelect = document.querySelector("#location");
-let parkTypeSelect = document.querySelector("#park-type");
-let parksContainer = document.querySelector(".parksContainer");
+window.onload = () => {
+  let searchSelect = document.querySelector("#searchSelect");
+  let locationSelect = document.querySelector("#location");
+  let parkTypeSelect = document.querySelector("#park-type");
+  let parksContainer = document.querySelector(".parksContainer");
 
-// hide park type select input
-parkTypeSelect.style.display = "none";
+  // hide "choose park type select"
+  parkTypeSelect.style.display = "none";
 
-// looping through locations array and put in locations select input
-locationsArray.forEach((location) => {
-  locationSelect.innerHTML += `<option value='${location}'>${location}</option>`;
-});
+   locationsArray.forEach((location) => {
+    locationSelect.innerHTML += `<option value="${location}">${location}</option>`;
+  });
 
-// looping through parkTypesArray and put in park type select input
-parkTypesArray.forEach((parkType) => {
-  parkTypeSelect.innerHTML += `<option value='${parkType}'>${parkType}</option>`;
-});
+  parkTypesArray.forEach((parkType) => {
+    parkTypeSelect.innerHTML += `<option value="${parkType}">${parkType}</option>`;
+  });
 
-// adding event listener so that when I can change the search type, js will know
-// and I can hide and show the locations select input
-searchType.addEventListener("change", () => {
-  if (searchType.value === "location") {
-    locationSelect.style.display = "inline-block";
-    parkTypeSelect.style.display = "none";
-  } else {
-    // hide and showing the location
-    locationSelect.style.display = "none";
-    parkTypeSelect.style.display = "inline-block";
+ 
+  searchSelect.addEventListener("change", searchSelectOnChange);
+
+  locationSelect.addEventListener("change", locationSelectOnchange);
+
+  parkTypeSelect.addEventListener("change", parkTypeSelectOnchange);
+
+  function searchSelectOnChange() {
+    if (searchSelect.value === "location") {
+      locationSelect.style.display = "inline-block";
+      parkTypeSelect.style.display = "none";
+    } else {
+      // hide and showing the location
+      locationSelect.style.display = "none";
+      parkTypeSelect.style.display = "inline-block";
+    }
   }
-});
 
-locationSelect.addEventListener("change", () => {
-  parksContainer.innerHTML = "";
-  const filteredParks = nationalParksArray.filter((park) =>
-    park.State.includes(locationSelect.value)
-  );
+  function locationSelectOnchange() {
+    parksContainer.innerHTML = "";
+    const filteredParks = nationalParksArray.filter((park) =>
+      park.State.includes(locationSelect.value)
+    );
 
-  for (let i = 0; i < filteredParks.length; i++) {
-    const park = filteredParks[i];
+    for (let i = 0; i < filteredParks.length; i++) {
+      const park = filteredParks[i];
 
-    parksContainer.innerHTML += `
+      parksContainer.innerHTML += `
+          <div class="parkCard">                               
+             <p>Location Name: ${park.LocationName}</p> 
+             <p>Address: ${park.Address}</p>
+             <p>City: ${park.City}</p>
+             <p>State: ${park.State}</p>
+             <p>Type: ${park.Location.type}</p>
+          </div>
+      `;
+    } // the back ticks are helping me because the "" wont work
+  }
+
+  function parkTypeSelectOnchange() {
+    parksContainer.innerHTML = "";
+    const filteredParks = nationalParksArray.filter((park) =>
+      park.LocationName.includes(parkTypeSelect.value)
+    );
+
+    for (let i = 0; i < filteredParks.length; i++) {
+      const park = filteredParks[i];
+
+      parksContainer.innerHTML += `
         <div class="parkCard">                               
            <p>Location Name: ${park.LocationName}</p> 
            <p>Address: ${park.Address}</p>
@@ -49,26 +74,6 @@ locationSelect.addEventListener("change", () => {
            <p>Type: ${park.Location.type}</p>
         </div>
     `;
-  } // the back ticks are helping me because the "" wont work
-});
-
-parkTypeSelect.addEventListener("change", () => {
-  parksContainer.innerHTML = "";
-  const filteredParks = nationalParksArray.filter((park) =>
-    park.LocationName.includes(parkTypeSelect.value)
-  );
-
-  for (let i = 0; i < filteredParks.length; i++) {
-    const park = filteredParks[i];
-
-    parksContainer.innerHTML += `
-        <div class="parkCard">                               
-           <p>Location Name: ${park.LocationName}</p> 
-           <p>Address: ${park.Address}</p>
-           <p>City: ${park.City}</p>
-           <p>State: ${park.State}</p>
-           <p>Type: ${park.Location.type}</p>
-        </div>
-    `;
+    }
   }
-});
+};
